1f5d598ae3cebd23c5adf1f9a776a7e83e29eae9
jerwski <kopia@unikolor.com>
2019-01-30 22:34:20 +0100

    refactoring

--- functions/myfunctions.py
+++ functions/myfunctions.py
@@ -66,6 +66,7 @@ def sendLeavesData(employee_id:int):
 
 def initial_worktime_form(employee_id:int, work_hours:int)->dict:
     '''return initial data for WorkEvidenceForm'''
+    data = dict(zip(range(1,5),range(12,19,2)))
     if date.today().isoweekday() == 1:
         start_date = date.today() - timedelta(days=3)
         if employee_id==10:
@@ -75,14 +76,7 @@ def initial_worktime_form(employee_id:int, work_hours:int)->dict:
         else:
             start_date = datetime(start_date.year, start_date.month,start_date.day,6,0)
             end_date = date.today() - timedelta(days=3)
-            if work_hours==0:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,12,0)
-            elif work_hours==1:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,14,0)
-            elif work_hours==2:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,16,0)
-            elif work_hours==3:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,18,0)
+            end_date = datetime(end_date.year, end_date.month, end_date.day,data[work_hours],0)
     else:
         start_date = date.today() - timedelta(days=1)
         if employee_id == 10:
@@ -92,14 +86,7 @@ def initial_worktime_form(employee_id:int, work_hours:int)->dict:
         else:
             start_date = datetime(start_date.year, start_date.month,start_date.day,6,0)
             end_date = date.today() - timedelta(days=1)
-            if work_hours==1:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,12,0)
-            elif work_hours==2:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,14,0)
-            elif work_hours==3:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,16,0)
-            elif work_hours==4:
-                end_date = datetime(end_date.year, end_date.month, end_date.day,18,0)
+            end_date = datetime(end_date.year, end_date.month, end_date.day,data[work_hours],0)
 
     context = {'start_work': start_date, 'end_work': end_date}
 
