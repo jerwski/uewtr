1331cf1cbf3eeed63fdeb0af1e42851da4ef4e60
jerwski <biuro@unikolor.com>
2020-06-10 18:29:42 +0200

    refactoring

--- employee/forms.py
+++ employee/forms.py
@@ -32,13 +32,15 @@ class EmployeeBasicDataForm(forms.ModelForm):
     DEACTIVE = 0
     ACTIVE = 1
     STATUS_CHOICE = ((DEACTIVE, 'Deactive'), (ACTIVE,'Active'))
+    # attrs
+    attrs={'class':'btn-sm btn-outline-light'}
     # fields
     forename = forms.CharField(max_length=100)
     surname = forms.CharField(max_length=100)
     pesel = forms.CharField(min_length=11, max_length=11)
-    leave = forms.ChoiceField(label="Leave choice:", widget=RadioSelectButtonGroup,
+    leave = forms.ChoiceField(label="Leave choice:", widget=RadioSelectButtonGroup(attrs=attrs),
                               required=True, choices=LEAVE_CHOICE)
-    status = forms.ChoiceField(label="Status choice:", widget=RadioSelectButtonGroup,
+    status = forms.ChoiceField(label="Status choice:", widget=RadioSelectButtonGroup(attrs=attrs),
                                required=True, choices=STATUS_CHOICE)
 
     class Meta:
@@ -52,16 +54,18 @@ class EmployeeExtendedDataForm(forms.ModelForm):
     WEEKLY_OVERTIME = 1
     SATURDAT_OVERTIME = 2
     RATINGS = [(NO_OVERTIME, 'Clear contract'), (WEEKLY_OVERTIME, 'Weekly overtime'), (SATURDAT_OVERTIME, 'Saturday overtime')]
+    # options, attrs, overclass
     options = {'icons': icons, 'useCurrent': True,
                'buttons': {'showToday': True, 'showClear': True, 'showClose': True}}
     attrs={'prepend': 'fa fa-calendar-check', 'append': 'fa fa-calendar-check', 'input_toggle': False, 'icon_toggle': True}
+    overclass={'class':'btn-sm btn-outline-light'}
     # fields
     worker = forms.ModelChoiceField(widget=forms.HiddenInput(attrs={'readonly': True}), queryset=queryset)
     birthday = forms.DateField(label='Date of birthday', widget=DatePicker(options=options, attrs=attrs))
     start_contract = forms.DateField(label='Date of start contract', widget=DatePicker(options=options, attrs=attrs))
     end_contract = forms.DateField(required=False, label='Date of end contract', widget=DatePicker(options=options, attrs=attrs))
-    overtime = forms.ChoiceField(label="Select the contract type...", required=True,
-                                 widget=RadioSelectButtonGroup, choices=RATINGS, initial=0)
+    overtime = forms.ChoiceField(label="Select the contract type:", required=True, initial=0,
+                                 widget=RadioSelectButtonGroup(attrs=overclass), choices=RATINGS)
     class Meta:
         model = EmployeeData
         fields = ('worker', 'birthday', 'postal', 'city', 'street', 'house', 'flat',
