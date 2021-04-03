607b28d1da77c0c8b92d318e9ef7c31499802728
jerwski <biuro@unikolor.com>
2021-04-03 20:30:10 +0200

    refactoring

--- uniwork/settings.py
+++ uniwork/settings.py
@@ -53,6 +53,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+	'django.contrib.admindocs',
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
